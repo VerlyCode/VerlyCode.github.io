0b99c67ff561364c20939b0912b76296250b6aae
VerlyCode <kimbatsaverlyfreedom@gmail.com>
2022-11-04 10:28:55 +0100

    Premiere_version

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbHost ='localhost';
+$dbUsername ='root';
+$dbPassword ='';
+$dbDatabase ='tutorial_db';
+$port =3308;
+$conn=mysqli_connect($dbHost.':'.$port,$dbUsername,$dbPassword,$dbDatabase);
+
+?>
